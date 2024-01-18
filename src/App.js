ae46b10bf3afca0a2cc8dbaf284ff8673660b855
Chen Wang <wangchen_ca@hotmail.com>
2024-01-17 19:35:18 -0800

    fetch data from my-mini-sb-app runing at localhost:4000

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 // App.js
 import React, { useState } from 'react';
 import './App.css';
-import candidatesData from './candidates';
+//import candidatesData from './candidates';
+import {useEffect} from 'react';
 import AvailableCandidatesTable from './AvailableCandidatesTable';
 import SelectedCandidatesTable from './SelectedCandidatesTable';
 
 function App() {
-  const [availableCandidates, setAvailableCandidates] = useState(candidatesData);
+  const [availableCandidates, setAvailableCandidates] = useState([]);
   const [selectedCandidates, setSelectedCandidates] = useState([]);
   const [disabledCandidates, setDisabledCandidates] = useState([]);
+
+  useEffect(() => {
+    // Define an async function to fetch data
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:4000/candidates');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log("fetched data=" + { data });
+        setAvailableCandidates(data);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    // Call the fetchData function
+    fetchData();
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+
+
+
 
   const handleCandidateSelect = (candidate) => {
     const updatedSelectedCandidates = [...selectedCandidates, candidate];
